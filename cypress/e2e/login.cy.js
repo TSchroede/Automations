64002f82deb3aee34e5659392a99a575d9afc466
TSchroede <tyl3r.schroeder+TB@gmail.com>
2022-06-21 19:12:58 -0500

    login test setup

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,15 @@
+describe('Login Page', () => {
+  beforeEach(function () {
+    cy.visit();
+    cy.url().should('include', 'account.tagboard.com/signin');
+    // exposing the login page
+    cy.contains('Continue with your email').click({ force: true });
+    // aliasing the inputs
+    cy.get('input[name=username]').as('username');
+    cy.get('input[name=password]').as('password');
+  });
+
+  context('invalid username states', function () {
+    it('test1', function () {});
+  });
+});
