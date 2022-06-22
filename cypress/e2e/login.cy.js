0c1a90d502a7b334903f351a769354eef8343a62
TSchroede <tyl3r.schroeder+TB@gmail.com>
2022-06-21 19:29:53 -0500

    Add failure for password and success test cases.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,5 @@
+const cypress = require('cypress');
+
 describe('Login Page', () => {
   beforeEach(function () {
     cy.visit();
@@ -5,30 +7,48 @@ describe('Login Page', () => {
     // exposing the login page
     cy.contains('Continue with your email').click({ force: true });
     // aliasing the inputs
-    cy.get('input[name=username]').as('username');
-    cy.get('input[name=password]').as('password');
+    cy.get('input[name=username]').should('be.visible').as('username');
+    cy.get('input[name=password]').should('be.visible').as('password');
   });
 
   context('invalid username states', function () {
     it('wrong username', function () {
-      cy.get('@username').should('be.visible').clear().type();
+      cy.get('@username').clear().type();
       cy.get('@password').clear().type();
-
       cy.contains('Wrong email or password').should('be.visible');
+      cy.url().should('include', 'account.tagboard.com/signin');
       cy.screenshot(`${Cypress.spec.name} : ${Cypress.currentTest.title}`);
     });
-    it('no username', function () {});
-    cy.get('@username').should('be.visible').clear();
-    cy.get('@password').clear().type();
-    cy.screenshot(`${Cypress.spec.name} : ${Cypress.currentTest.title}`);
+    it('no username', function () {
+      cy.get('@username').clear();
+      cy.get('@password').clear().type();
+      cy.url().should('include', 'account.tagboard.com/signin');
+      cy.screenshot(`${Cypress.spec.name} : ${Cypress.currentTest.title}`);
+    });
   });
 
   context('invalid password states', function () {
-    it('wrong password', function () {});
-    it('no password', function () {});
+    it('wrong password', function () {
+      cy.get('@username').clear().type();
+      cy.get('@password').clear().type();
+      cy.url().should('include', 'account.tagboard.com/signin');
+      cy.screenshot(`${Cypress.spec.name} : ${Cypress.currentTest.title}`);
+    });
+
+    it('no password', function () {
+      cy.get('@username').clear().type();
+      cy.get('@password').clear();
+      cy.url().should('include', 'account.tagboard.com/signin');
+      cy.screenshot(`${Cypress.spec.name} : ${Cypress.currentTest.title}`);
+    });
   });
 
   context('success', function () {
-    it('sucessful login', function () {});
+    it('sucessful login', function () {
+      cy.get('@username').clear().type();
+      cy.get('@password').clear().type();
+      cy.url().should('include', '');
+      cy.screenshot(`${Cypress.spec.name} : ${Cypress.currentTest.title}`);
+    });
   });
 });
